80f748783bb99d3d9819d5bb5350ffda2b3f208b
adriendelagneau <delagneauadrien@yahoo.fr>
2024-01-31 20:01:00 +0100

    first commit

--- components/providers/ThemeToggleProviser.jsx
+++ components/providers/ThemeToggleProviser.jsx
@@ -1,19 +1,12 @@
 "use client"
 
 import { ThemeProvider } from 'next-themes'
-import React, { useEffect, useState } from 'react'
 
 const ThemeToggleProviser = ({ children }) => {
     
-    const [isMounted, setIsMounted] = useState(false)
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
-
-    if (!isMounted) return <>{children}</>
   return (
-      <ThemeProvider attribute="class">{ children }</ThemeProvider>
+      <ThemeProvider attribute="class" defaultTheme='system' enableSystem>{ children }</ThemeProvider>
   )
 }
 
